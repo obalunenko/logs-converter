207d0bddd21af205c7d80f9dc8fdf824d3e81c60
Oleg Balunenko <oleg.balunenko@gmail.com>
2018-08-17 15:58:00 +0000

    statistics info

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"os/signal"
+	"sync/atomic"
 	"syscall"
+	"text/tabwriter"
 
 	log "github.com/sirupsen/logrus"
 	"gitlab.com/oleg.balunenko/logs-converter/config"
@@ -34,25 +37,42 @@ func main() {
 	signals := make(chan os.Signal, 1)
 	signal.Notify(signals, os.Interrupt)
 	signal.Notify(signals, syscall.SIGTERM)
-	var storedModelsCnt, failedToStoreCnt int
+	var storedModelsCnt, failedToStoreCnt, totalRecCnt uint64
+
 	for {
 
 		select {
 		case <-signals:
 			log.Infof("Got UNIX signal, shutting down")
 			mongo.CloseConnection(dbCollection)
-			log.Infof("Total stored logs in DB: [%d]", storedModelsCnt)
-			log.Infof("Total failed to store logs in DB: [%d]", failedToStoreCnt)
+
+			w := new(tabwriter.Writer)
+			w.Init(os.Stdout, 0, 0, 0, ' ', tabwriter.Debug|tabwriter.AlignRight)
+			_, err := fmt.Fprintf(w, "Execution statistics:\n"+
+				"Total models recived\tStored in DB\tFailed to store in DB\n"+
+				"%d\t%d\t%d", totalRecCnt, storedModelsCnt, failedToStoreCnt)
+			if err != nil {
+				log.Fatalf("Failed to print statistic: %v", err)
+			}
+			//fmt.Fprintln(w)
+			if err := w.Flush(); err != nil {
+				log.Fatalf("Failed to flush statistic writer: %v", err)
+			}
+
 			return
 
 		case data := <-resChan:
+
+			atomic.AddUint64(&totalRecCnt, 1)
 			log.Debugf("Received model: %+v", data)
+			log.Infof("Current amount of recieved models to store is: [%d]", totalRecCnt)
 			errStore := mongo.StoreModel(data, dbCollection)
 			if errStore != nil {
 				log.Errorf("Failed to store model...: %v", errStore)
-				failedToStoreCnt++
+				atomic.AddUint64(&failedToStoreCnt, 1)
 			} else {
-				storedModelsCnt++
+				atomic.AddUint64(&storedModelsCnt, 1)
+
 			}
 
 		}
