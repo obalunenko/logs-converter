50b7af4bf7362ab96c72973c104e14ebea1a4212
Oleg Balunenko <obalunenko@partners.luno.com>
2018-12-08 19:45:30 +0300

    config: refactor config tests

--- config/config_test.go
+++ config/config_test.go
@@ -6,33 +6,36 @@ import (
 	"path/filepath"
 	"reflect"
 	"testing"
-
-	log "github.com/sirupsen/logrus"
 )
 
-func testConfigCreate(configPath string, logsFilesListJSON string, logLevel string, mongoURL string, mongoDB string, mongoCollection string, dropDB bool) error {
+func testConfigCreate(t *testing.T, configPath string, logsFilesListJSON string, logLevel string, mongoURL string, mongoDB string,
+	mongoCollection string, dropDB bool) error {
+
 	fmt.Println("Helper func in action")
 	if err := os.MkdirAll(filepath.Dir(configPath), 0700); err != nil {
-		log.Fatalf("failed creating all dirs for config file [%s]: %v", filepath.Dir(configPath), err)
+		t.Fatalf("failed creating all dirs for config file [%s]: %v", filepath.Dir(configPath), err)
 
 	}
 
 	configFile, err := os.OpenFile(configPath, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0777)
+
 	if err != nil {
-		log.Fatalf("error opening config file: %v", err)
+		t.Fatalf("error opening config file: %v", err)
 	}
 
 	if err != nil {
-		return fmt.Errorf("Failed to create test config: %v", err)
+		return fmt.Errorf("failed to create test config: %v", err)
 	}
+
 	_, err = configFile.WriteString(fmt.Sprintf(`LogLevel="%s"
 	LogsFilesListJSON='%s'
 	MongoURL="%s"
 	MongoDB="%s"
 	MongoCollection="%s"
 	DropDB=%t`, logLevel, logsFilesListJSON, mongoURL, mongoDB, mongoCollection, dropDB))
+
 	if err != nil {
-		return fmt.Errorf("Failed to write test config: %v", err)
+		return fmt.Errorf("failed to write test config: %v", err)
 	}
 	if err := configFile.Close(); err != nil {
 		return fmt.Errorf("failed to close test config file descriptor: %v", err)
@@ -41,10 +44,10 @@ func testConfigCreate(configPath string, logsFilesListJSON string, logLevel stri
 	return nil
 }
 
-func testConfigDelete(configPath string) {
+func testConfigDelete(t *testing.T, configPath string) {
 	err := os.Remove(configPath)
 	if err != nil {
-		log.Fatalf("Failed to delete old config file")
+		t.Fatalf("Failed to delete old config file")
 	}
 }
 
@@ -63,15 +66,13 @@ func TestLoadConfig(t *testing.T) {
 		wantConfig *Config
 		wantErr    bool
 	}
-	type testCase struct {
+	var tests = []struct {
 		id             int
 		description    string
 		input          input
 		expectedResult expectedResult
-	}
-	type testSuite []testCase
-	var ts = testSuite{
-		testCase{
+	}{
+		{
 			id:          1,
 			description: `Check configuration loading from cofig file`,
 			input: input{
@@ -99,7 +100,7 @@ func TestLoadConfig(t *testing.T) {
 				wantErr: false,
 			},
 		},
-		testCase{
+		{
 			id:          2,
 			description: `Broken config: incorrect json with files`,
 			input: input{
@@ -117,7 +118,7 @@ func TestLoadConfig(t *testing.T) {
 				wantErr:    true,
 			},
 		},
-		testCase{
+		{
 			id:          3,
 			description: `Broken config: empty json with files`,
 			input: input{
@@ -136,23 +137,24 @@ func TestLoadConfig(t *testing.T) {
 			},
 		},
 	}
+
 	currentDir, err := filepath.Abs(filepath.Dir(os.Args[0]))
 	if err != nil {
-		log.Fatal(err)
+		t.Fatal(err)
 	}
-	configPath := filepath.Join(currentDir, "config.toml")
+	configPath := filepath.Join(currentDir, "testdata", "config.toml")
 
-	for _, tc := range ts {
+	for _, tc := range tests {
 
 		t.Run(fmt.Sprintf("Test%d:%s", tc.id, tc.description), func(t *testing.T) {
 
-			err = testConfigCreate(configPath, tc.input.logsFilesListJSON, tc.input.logLevel, tc.input.mongoURL, tc.input.mongoDB, tc.input.mongoCollection, tc.input.dropDB)
+			err = testConfigCreate(t, configPath, tc.input.logsFilesListJSON, tc.input.logLevel, tc.input.mongoURL, tc.input.mongoDB, tc.input.mongoCollection, tc.input.dropDB)
 			if err != nil {
 				t.Fatalf("Error while creating test config: %v", err)
 			}
 
 			gotModel, err := LoadConfig(configPath)
-			testConfigDelete(configPath) // Delete old config file after it was loaded
+			testConfigDelete(t, configPath) // Delete old config file after it was loaded
 
 			if (err != nil) != tc.expectedResult.wantErr {
 				t.Errorf("processLine() error = %+v, \nwantErr %+v", err, tc.expectedResult.wantErr)
