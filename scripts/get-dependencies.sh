ec4418394a93800ea8dc1d52e8c5504cee27913a
Oleg Balunenko <obalunenko@partners.luno.com>
2018-12-25 16:38:18 +0300

    scripts: refactor and update scripts

--- scripts/get-dependencies.sh
+++ scripts/get-dependencies.sh
@@ -1,20 +1,22 @@
 #!/usr/bin/env bash
 
 function get_dependencies() {
-      declare -a packages=(
-"golang.org/x/tools/cmd/cover/..."
-"github.com/mattn/goveralls/..."
-"github.com/alecthomas/gometalinter/..."
-"golang.org/x/lint/golint"
-"github.com/Bubblyworld/gogroup/..."
-)
-
-## now loop through the above array
-for pkg in "${packages[@]}"
-do
-   echo "$pkg"
-   go get -u -v "$pkg"
-done
+    declare -a packages=(
+        "golang.org/x/tools/cmd/cover/..."
+        "github.com/mattn/goveralls/..."
+        "github.com/Bubblyworld/gogroup/..."
+    )
+    
+    ## now loop through the above array
+    for pkg in "${packages[@]}"
+    do
+        echo "$pkg"
+        go get -u -v "$pkg"
+    done
+    
+    curl -sfL https://install.goreleaser.com/github.com/golangci/golangci-lint.sh | sh -s -- -b $(go env GOPATH)/bin v1.12.5
+    golangci-lint --version
+    
 }
 
 
